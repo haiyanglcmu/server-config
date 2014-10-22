4217cb00fde3c5a72cb88465e984f2fbb180e9ef
Haiyang Li <lihaiyang.email@yahoo.com>
2014-10-23 02:13:40 +0800

    update

--- compile-vim.sh
+++ compile-vim.sh
@@ -6,9 +6,10 @@
   --enable-multibyte \
   --enable-rubyinterp \
   --enable-luainterp \
+  --with-lua-prefix=/usr --with-luajit \
   --enable-pythoninterp \
+  --with-python-config-dir=/usr/local/lib/python2.7/config/ \
   --enable-cscope \
   --enable-perlinterp \
-  --with-luajit \
   --enable-fontset \
   --enable-fail-if-missing
