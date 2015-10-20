1b22b7ca4c607636fce5cb7e82b7ada0be700cf9
EC2 Default User <ec2-user@ip-172-31-59-11.ec2.internal>
2015-10-20 01:13:01 +0000

    update

--- server-config.sh
+++ server-config.sh
@@ -79,17 +79,21 @@ vim_install_dir=$home_dir/opt/vim74
 # configure vim
 
 ## link vimrc and vimfiles
+echo "### creating symbolic links to vimrc and vimfiles ..."
 ln -s $config_dir/vimrc $home_dir/.vimrc
 if ! [[ -e "$home_dir/.vim" ]]; then
   ln -s $config_dir/vimfiles $home_dir/.vim
 fi
 
 ## get neobundle
+echo "### cloning neobundle ..."
 mkdir -p ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim $home_dir/.vim/bundle/neobundle.vim
 
 ## install powerline fonts
+echo "### cloning powerline fonts ..."
 git clone https://github.com/Lokaltog/powerline-fonts.git $config_dir/vimfiles/powerline-fonts
 $config_dir/vimfiles/powerline-fonts/install.sh
 
+echo "### installing neobundle plugins ..."
 ~/.vim/bundle/neobundle.vim/bin/neoinstall
